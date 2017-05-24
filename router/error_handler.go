d7fd38718c8e3692a25c3d2dad610a3a17208cbc
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2017-05-24 14:47:53 +1000

    Fixed bad template execute code.

--- router/error_handler.go
+++ router/error_handler.go
@@ -7,17 +7,17 @@ import (
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	err := notFoundTpl.Execute(w, nil)
+	err := notFoundTpl.ExecuteTemplate(w, "base.gohtml", nil)
 	if err != nil {
 		log.Println(err)
+		errorHandler(w, r)
 	}
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	err := errorTpl.Execute(w, nil)
+	w.WriteHeader(http.StatusInternalServerError)
+	err := errorTpl.ExecuteTemplate(w, "base.gohtml", nil)
 	if err != nil {
 		log.Println(err)
 	}
 }
-
