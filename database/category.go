6afd5ba124b2c3b01f1ec3f5e94b1c462e3ccba7
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2017-06-15 16:25:08 +1000

    Moved code for more clarity

--- database/category.go
+++ database/category.go
@@ -10,6 +10,15 @@ func GetAllCategories() []model.Category {
 	return categoryData
 }
 
+func GetCategoryBySlug(slug string) (c model.Category, err error) {
+	for _, categ := range GetAllCategories() {
+		if genSlug(categ.Name) == slug {
+			return categ, nil
+		}
+	}
+	return c, errors.New("Category does not exist.")
+}
+
 func GetAllCategoriesWithCalculations() []model.CategoryWithCalculations {
 	return categoriesData
 }
@@ -21,13 +30,4 @@ func GetCategoryWithCalculationsBySlug(slug string) (c model.CategoryWithCalcula
 	}
 	calcs, _ := GetCalculationsByCategorySlug(slug)
 	return model.CategoryWithCalculations{Category: categ, Calculations: calcs}, nil
-}
-
-func GetCategoryBySlug(slug string) (c model.Category, err error) {
-	for _, categ := range GetAllCategories() {
-		if genSlug(categ.Name) == slug {
-			return categ, nil
-		}
-	}
-	return c, errors.New("Category does not exist.")
 }
