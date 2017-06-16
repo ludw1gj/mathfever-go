778ff0ac3a9514a95328f00dc7bb67e791f10a58
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2017-06-16 11:25:19 +1000

    Organised and removed unused static files

--- database/category_data.go
+++ database/category_data.go
@@ -12,19 +12,19 @@ var (
 	}
 	numbers = model.Category{
 		"Primes and Factors",
-		"/static/resource/image/category/addition.jpg",
+		"/static/resource/image/category/numbers.jpg",
 		"Calculations about numbers! Find Highest Common Factor, find Lowest Common Multiple, and " +
 			"figuring out Prime Numbers.",
 	}
 	percentages = model.Category{
 		"Percentages",
-		"/static/resource/image/category/algebra.jpg",
+		"/static/resource/image/category/percentages.jpg",
 		"Calculations for percentages! Find the value from a percentage, find a percentage from a " +
 			"value, or find the percentage change between two values.",
 	}
 	tsa = model.Category{
 		"Total Surface Area",
-		"/static/resource/image/category/geometry.jpg",
+		"/static/resource/image/category/tsa.jpg",
 		"Calculations that you might use for Total Surface Area: Pythagorean Theorem (also known as " +
 			"Pythagoras's Theorem), Total Surface Area of Cone, Total Surface Area of Cube, Total Surface " +
 			"Area of Cylinder, Total Surface Area of Rectangular Prism, Total Surface Area of Sphere, and " +
