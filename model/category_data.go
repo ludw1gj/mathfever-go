7768cb7a281fa6e325f224a5fe85277061038712
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2018-01-30 13:12:38 +1100

    Standardise static folder names

--- model/category_data.go
+++ model/category_data.go
@@ -4,26 +4,26 @@ var (
 	categories = []Category{
 		{
 			"Networking",
-			"/static/resource/image/category/networking.jpg",
+			"/static/resource/images/category/networking.jpg",
 			"Calculations that you might use for Networking/Computer Science: Binary to Decimal, Binary " +
 				"to Hexadecimal, Decimal to Binary, Decimal to Hexadecimal, Hexadecimal to Binary, and " +
 				"Hexadecimal to Decimal.",
 		},
 		{
 			"Primes and Factors",
-			"/static/resource/image/category/numbers.jpg",
+			"/static/resource/images/category/numbers.jpg",
 			"Calculations about numbers! Find Highest Common Factor, find Lowest Common Multiple, and " +
 				"figuring out Prime Numbers.",
 		},
 		{
 			"Percentages",
-			"/static/resource/image/category/percentages.jpg",
+			"/static/resource/images/category/percentages.jpg",
 			"Calculations for percentages! Find the value from a percentage, find a percentage from a " +
 				"value, or find the percentage change between two values.",
 		},
 		{
 			"Total Surface Area",
-			"/static/resource/image/category/tsa.jpg",
+			"/static/resource/images/category/tsa.jpg",
 			"Calculations that you might use for Total Surface Area: Pythagorean Theorem (also known as " +
 				"Pythagoras's Theorem), Total Surface Area of Cone, Total Surface Area of Cube, Total Surface " +
 				"Area of Cylinder, Total Surface Area of Rectangular Prism, Total Surface Area of Sphere, and " +
